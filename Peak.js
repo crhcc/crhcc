bcc5b08dc8904b7c947febbff31bfa3d3300d988
crhcc <62683111+crhcc@users.noreply.github.com>
2022-06-29 09:59:52 +0800

    Update Peak.js

--- Peak.js
+++ Peak.js
@@ -12,34 +12,36 @@ hostname:billing.peakcloud.org
 var obj = JSON.parse($response.body);
  
 obj = {
-     "metaResponse": {
-    "code": 0
-  },
-
-     "response": {
-    "bbuid": "NKoKXsjJnw",
-    "usedIntroOfferPeriod": false,
-    "class": "UserModuleBillingResponse",
-    "is_in_billing_retry": false,
-    "modules": [{
-      "class": "BillingModuleResponse",
-      "name": "com.brainbow.module.peak.PeakModule",
-      "subscription": {
-        "status": 0,
-        "statusdate": 0,
-        "source": {
-          "id": "com.brainbow.peak.arsub_1wtrial_12m3499",
-          "cancelable": false,
-          "provider": "itunes",
-          "type": "trial"
-        },
-        "bbuid": "NKoKXsjJnw",
-        "endTime": 4070956030000,
-        "startTime": 1577884030503,
-        "pro": true
-      }
-    }]
+ "response": {
+  "bbuid": "ZPpZvQbStP",
+  "usedIntroOfferPeriod": false,
+  "class": "UserModuleBillingResponse",
+  "is_in_billing_retry": false,
+  "modules": [
+   {
+    "class": "BillingModuleResponse",
+    "name": "com.brainbow.module.peak.PeakModule",
+    "subscription": {
+     "status": 0,
+     "statusdate": 0,
+     "source": {
+      "id": "com.brainbow.peak.arsub_1wtrial_12m4968",
+      "cancelable": false,
+      "provider": "itunes",
+      "type": "trial"
+     },
+     "bbuid": "ZPpZvQbStP",
+     "endTime": 1779061000000,
+     "startTime":1531710600000,
+     "pro": true
+    }
+   }
+  ]
+ },
+ "metaResponse": {
+  "code": 0
  }
 }
+
 
 $done({body: JSON.stringify(obj)});
